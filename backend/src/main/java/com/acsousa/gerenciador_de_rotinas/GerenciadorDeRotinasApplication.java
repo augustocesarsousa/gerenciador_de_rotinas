93aa633200730c7d6b7701c315911a4b97de30f6
Augusto Cesar <acs-03@hotmail.com>
2025-11-09 12:14:39 -0300

    feat: backend iniciado

---
+++ backend/src/main/java/com/acsousa/gerenciador_de_rotinas/GerenciadorDeRotinasApplication.java
@@ -0,0 +1,13 @@
+package com.acsousa.gerenciador_de_rotinas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeRotinasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeRotinasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/acsousa/gerenciador_de_rotinas/GerenciadorDeRotinasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acsousa.gerenciador_de_rotinas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeRotinasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
