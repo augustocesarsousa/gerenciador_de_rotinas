a89a7cba8b7d04d69912e1be76bbf4e153235d5e
Augusto Cesar <acs-03@hotmail.com>
2026-01-22 15:20:15 -0300

    feat: cria a estrutura de pastas do sistema e a tela do login

---
+++ frontend/src/app/features/login/login.routes.ts
@@ -0,0 +1,9 @@
+import { Route } from '@angular/router';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+
+export const routes: Route[] = [
+  {
+    path: '',
+    component: LoginPageComponent,
+  },
+];
